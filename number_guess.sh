b70341dd4844cc14fafb9604e2f9153dd4d5f8c6
BlackZerberus <c.e.chappa@gmail.com>
2022-06-03 18:45:53 +0000

    feat: add prompt username

--- number_guess.sh
+++ number_guess.sh
@@ -6,6 +6,8 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 # generate a random number up to 1000:
 NUMBER=$(($RANDOM % 1000))
 # Prompt the user for a username:
+echo -e "Enter your username:\n"
+read USERNAME
 # If it's been registered before: (Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.)
 # If it's the user's first time:
 # Guess the secret number between 1 and 1000:
