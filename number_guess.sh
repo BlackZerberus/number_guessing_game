a7ba3909467ad4a3ad337da182f95e5a4afd2ecb
BlackZerberus <c.e.chappa@gmail.com>
2022-06-03 18:54:58 +0000

    feat: add random number

--- number_guess.sh
+++ number_guess.sh
@@ -3,6 +3,8 @@
 # psql string connection:
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+# generate a random number up to 1000:
+NUMBER=$(($RANDOM % 1000))
 # Prompt the user for a username:
 # If it's been registered before: (Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.)
 # If it's the user's first time:
