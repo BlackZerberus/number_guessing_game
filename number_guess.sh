0af89f794f901eeb2d2078b9906c5ba18ea82cf2
BlackZerberus <c.e.chappa@gmail.com>
2022-06-03 18:02:06 +0000

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# psql string connection:
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Prompt the user for a username:
+# If it's been registered before:
+# If it's the user's first time:
+# Guess the secret number between 1 and 1000:
+# if is not the correct answer:
+# if the correct number is guessed:
